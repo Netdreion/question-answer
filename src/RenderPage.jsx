cb966aee30a9c68680a860ed6a2755330ada244d
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-10 21:45:49 -0400

    mapped over the data for display

--- src/RenderPage.jsx
+++ src/RenderPage.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import questions from "./Data";
 
 const RenderPage = () => {
+  const [question, setQuestion] = useState(questions);
+
   return (
     <>
       <div className="container">
         <h1>questions and info </h1>
         <div className="question-container">
           <ul>
-            <li></li>
+            {question.map((item) => {
+              return (
+                <li key={question.id}>
+                  <h5>
+                    {item.title}
+                    <p> {item.info}</p>
+                  </h5>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
     </>
   );
 };
+
+export default RenderPage;
